85d82230fd19444f68d5580b4d1082d6a5f01d3b
KayJayx <73000710+KayJayx@users.noreply.github.com>
2025-06-13 20:57:10 -0700

    Added Drag and Drop Capability

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import DndProviderWrapper from './dnd/DndProviderWrapper.jsx'
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*
+    The drag and drop capabaility can only be added if the App is wrapped
+    around react's drag and drop and HTML drag and drop components.
+    */}
+    <DndProviderWrapper>
+      <App />
+    </DndProviderWrapper>
   </StrictMode>,
 )
