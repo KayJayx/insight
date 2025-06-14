4b246d15b1a0cf46f537a7964d6ed11e22c5d416
KayJayx <73000710+KayJayx@users.noreply.github.com>
2025-06-13 18:31:11 -0700

    Create SidePanel.jsx

---
+++ src/components/SidePanel.jsx
@@ -0,0 +1,11 @@
+function SidePanel() {
+  return (
+    <div className="side-panel">
+      <h3 className="components-title">Components</h3>
+      <button className="icon-button">New Plot</button>
+      <button className="icon-button">New Text</button>
+    </div>
+  )
+}
+
+export default SidePanel
