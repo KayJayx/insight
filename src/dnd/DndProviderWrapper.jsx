6fb302f194c5ff669377c31df91a172fa6830935
KayJayx <73000710+KayJayx@users.noreply.github.com>
2025-06-14 19:59:30 -0700

    Added Comment

--- src/dnd/DndProviderWrapper.jsx
+++ src/dnd/DndProviderWrapper.jsx
@@ -2,6 +2,8 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
 
 function DndProviderWrapper({ children }) {
+  // This component wraps the application in a React DnD provider
+  // using the HTML5 backend, enabling drag-and-drop support in the browser.
   return <DndProvider backend={HTML5Backend}>{children}</DndProvider>;
 }
 
