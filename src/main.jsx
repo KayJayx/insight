28b9ec7321aed8719a8d65e1480c6d8f13490f63
KayJayx <73000710+KayJayx@users.noreply.github.com>
2025-06-15 17:58:48 -0700

    Removed The Old Dnd Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import DndProviderWrapper from './dnd/DndProviderWrapper.jsx'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/*
-    The drag and drop capabaility can only be added if the App is wrapped
-    around HTML/react's drag and drop components.
-    */}
-    <DndProviderWrapper>
+    {/* DndContext provides drag-and-drop capabilities using dnd-kit */}
+    <DndContext>
       <App />
-    </DndProviderWrapper>
+    </DndContext>
   </StrictMode>,
 )
